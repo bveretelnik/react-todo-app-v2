ac9f70bef83ed5a9fe196a23fc02849ad3de6989
Bogdan <bveretelnik@gmail.com>
2020-11-28 17:56:40 +0200

    upload components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-import React from "react";
+import { Button, TextField } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { fb } from "./firebase";
+import firebase from "firebase/app";
+import Todo from "./Todo/Todo";
 import "./App.css";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+
+  useEffect(() => {
+    getTodos();
+    //eslint-disable-next-line
+  }, []);
+
+  const getTodos = () => {
+    fb.collection("todos").onSnapshot((querySnapshot) => {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      );
+    });
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    fb.collection("todos").add({
+      inprogress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    });
+    setTodoInput("");
+  };
+
   return (
-    <div>
-      <h1>B_Veretelnik TODO APP 🔥</h1>
+    <div className="App">
+      <div className="container">
+        <h1>B_Veretelnik TODO APP 🔥</h1>
+        <form>
+          <TextField
+            id="standard-basic"
+            label="Write a Todo"
+            value={todoInput}
+            style={{ width: "90vw", maxWidth: "500px" }}
+            onChange={(e) => setTodoInput(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={addTodo}
+            style={{ display: "none" }}
+          >
+            Default
+          </Button>
+        </form>
+        <div className="container2">
+          {todos.map((todo) => (
+            <Todo todo={todo.todo} inprogress={todo.inprogress} id={todo.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,5 +1,29 @@
+import { Button, ListItem, ListItemText } from "@material-ui/core";
 import React from "react";
+import { fb } from "../firebase";
 
-export default function Todo() {
-  return <div></div>;
+export default function Todo({ todo, inprogress, id }) {
+  const toggleInProgress = () => {
+    fb.collection("todos").doc(id).update({
+      inprogress: !inprogress,
+    });
+  };
+  const deleteTodo = () => {
+    fb.collection("todos").doc(id).delete();
+  };
+  return (
+    <div style={{ display: "lex" }}>
+      <ListItem>
+        <ListItemText
+          primary={todo}
+          secondary={inprogress ? "In Progress..." : "Completed"}
+        />
+      </ListItem>
+
+      <Button onClick={toggleInProgress}>
+        {inprogress ? "Done" : "UnDone"}
+      </Button>
+      <Button onClick={deleteTodo}>X</Button>
+    </div>
+  );
 }
